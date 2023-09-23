775b9bd0e41aca96ac7e8d42c775dba77d08be86
LegaSilverskiy <slepchenko98@yandex.ru>
2023-09-23 14:29:42 +0500

    add actions for numbers button and added the ability to enter text in UItextView

--- Calc/ViewController.swift
+++ Calc/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var fieldForNumbers: UITextView!
     
     @IBOutlet weak var zeroButton: UIButton!
@@ -30,14 +30,90 @@ class ViewController: UIViewController {
     @IBOutlet weak var divisionButton: UIButton!
     @IBOutlet weak var percentButton: UIButton!
     @IBOutlet weak var changeNegativeOrPositiveButton: UIButton!
-    @IBOutlet weak var cleatButton: UIButton!
+    @IBOutlet weak var clearButton: UIButton!
     
+    private var number : String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        fieldForNumbers.backgroundColor = .black
+        fieldForNumbers.textColor = .white
+        fieldForNumbers.text = String(number)
     }
-
-
+    @IBAction func tapZeroButton(_ sender: Any) {
+        let numberZero = number + "0"
+        number = numberZero
+        fieldForNumbers.text = String(number)
+    }
+    @IBAction func tapOneButton(_ sender: Any) {
+        let numberOne = number + "1"
+        number = numberOne
+        fieldForNumbers.text = String(number)
+    }
+    @IBAction func tapTwoButton(_ sender: Any) {
+        let numberTwo = number + "2"
+        number = numberTwo
+        fieldForNumbers.text = String(number)
+    }
+    @IBAction func tapThreeButton(_ sender: Any) {
+        let numberThree = number + "3"
+        number = numberThree
+        fieldForNumbers.text = String(number)
+    }
+    @IBAction func tapFourButton(_ sender: Any) {
+        let numberFour = number + "4"
+        number = numberFour
+        fieldForNumbers.text = String(number)
+    }
+    @IBAction func tapFiveButton(_ sender: Any) {
+        let numberFive = number + "5"
+        number = numberFive
+        fieldForNumbers.text = String(number)
+    }
+    @IBAction func tapSixButton(_ sender: Any) {
+        let numberSix = number + "6"
+        number = numberSix
+        fieldForNumbers.text = String(number)
+    }
+    @IBAction func tapSevenButton(_ sender: Any) {
+        let numberSeven = number + "7"
+        number = numberSeven
+        fieldForNumbers.text = String(number)
+    }
+    @IBAction func tapEightButton(_ sender: Any) {
+        let numberEight = number + "8"
+        number = numberEight
+        fieldForNumbers.text = String(number)
+    }
+    @IBAction func tapNineButton(_ sender: Any) {
+        let numberNine = number + "9"
+        number = numberNine
+        fieldForNumbers.text = String(number)
+    }
+    @IBAction func tapCommaButton(_ sender: Any) {
+        let comma = number + ","
+        number = comma
+        fieldForNumbers.text = String(number)
+    }
+    
+    @IBAction func tapEqualityButton(_ sender: Any) {
+    }
+    @IBAction func tapPlusButton(_ sender: Any) {
+    }
+    @IBAction func tapMinusButton(_ sender: Any) {
+    }
+    @IBAction func tapMultiplicationButton(_ sender: Any) {
+    }
+    @IBAction func tapDivisionButton(_ sender: Any) {
+    }
+    @IBAction func tapPercentButton(_ sender: Any) {
+    }
+    @IBAction func tapChangeNegativeOrPositiveButton(_ sender: Any) {
+    }
+    @IBAction func tapClearButton(_ sender: Any) {
+    }
+    
+    
 }
 
