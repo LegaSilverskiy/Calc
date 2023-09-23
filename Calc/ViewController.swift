52dd5fcbdbeae8506c94d831168b35e8bfb84c95
LegaSilverskiy <slepchenko98@yandex.ru>
2023-09-23 13:38:26 +0500

    Add all interface buttons, and field for numbers Add all outlets for buttons, and UItextView

--- Calc/ViewController.swift
+++ Calc/ViewController.swift
@@ -9,6 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fieldForNumbers: UITextView!
+    
+    @IBOutlet weak var zeroButton: UIButton!
+    @IBOutlet weak var oneButton: UIButton!
+    @IBOutlet weak var twoButton: UIButton!
+    @IBOutlet weak var threeButton: UIButton!
+    @IBOutlet weak var fourButton: UIButton!
+    @IBOutlet weak var fiveButton: UIButton!
+    @IBOutlet weak var sixButton: UIButton!
+    @IBOutlet weak var sevenButton: UIButton!
+    @IBOutlet weak var eightButton: UIButton!
+    @IBOutlet weak var nineButton: UIButton!
+    @IBOutlet weak var commaButton: UIButton!
+    
+    @IBOutlet weak var equalityButton: UIButton!
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var multiplicationButton: UIButton!
+    @IBOutlet weak var divisionButton: UIButton!
+    @IBOutlet weak var percentButton: UIButton!
+    @IBOutlet weak var changeNegativeOrPositiveButton: UIButton!
+    @IBOutlet weak var cleatButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
